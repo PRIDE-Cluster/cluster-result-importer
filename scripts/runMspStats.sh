96f294fb4228eb2da92ab117bde672bbb14bac8d
Rui Wang <harry.r.wang@gmail.com>
2015-08-24 16:46:55 +0100

    fix and document msp stats shell script

--- scripts/runMspStats.sh
+++ scripts/runMspStats.sh
@@ -7,6 +7,7 @@ echo -ne "Filename\ttotal_spectra\tunique_sequences\n"
 for MSP_FILE in "$@"; do
         UNIQUE_SEQUENCES=`zgrep "^Name:" "$MSP_FILE" | sed 's|Name: \(.*\)/.*|\1|' | sort | uniq | wc -l`
         SPECTRA=`zgrep -c "^Name:" "$MSP_FILE"`
+        BASENAME=`basename "$MSP_FILE"`
 
-        echo -e "$MSP_FILE\t$SPECTRA\t$UNIQUE_SEQUENCES"
+        echo -e "$BASENAME\t$SPECTRA\t$UNIQUE_SEQUENCES"
 done
