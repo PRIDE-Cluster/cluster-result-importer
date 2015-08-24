74d13b964ac0b6e6c4ae178447a45bd1431f6323
Rui Wang <harry.r.wang@gmail.com>
2015-08-24 16:41:07 +0100

    fix and document msp stats shell script

--- scripts/runMspStats.sh
+++ scripts/runMspStats.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# The input parameter should be in the format of
+# /path/to/spectra/libraries/*
+
 echo -ne "Filename\ttotal_spectra\tunique_sequences\n"
 for MSP_FILE in "$@"; do
-        UNIQUE_SEQUENCES=`grep "^Name:" "$MSP_FILE" | sed 's|Name: \(.*\)/.*|\1|' | sort | uniq | wc -l`
-        SPECTRA=`grep -c "^Name:" "$MSP_FILE"`
+        UNIQUE_SEQUENCES=`zgrep "^Name:" "$MSP_FILE" | sed 's|Name: \(.*\)/.*|\1|' | sort | uniq | wc -l`
+        SPECTRA=`zgrep -c "^Name:" "$MSP_FILE"`
 
         echo -e "$MSP_FILE\t$SPECTRA\t$UNIQUE_SEQUENCES"
 done
